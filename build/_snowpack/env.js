8bb7577885d01e58037bb450d6b99c9df87f3e26
Emeka Orji <emekapraiseo@gmail.com>
2022-06-17 07:36:19 +0100

    build: ran 'npm run build'

---
+++ build/_snowpack/env.js
@@ -0,0 +1,4 @@
+export const API_URL = "myoneandonlysecretapikey";
+export const MODE = "production";
+export const NODE_ENV = "production";
+export const SSR = false;
--- build/dist/index.js
+++ build/dist/index.js
@@ -1,1 +1,17 @@
-import e from"../_snowpack/pkg/canvas-confetti.js";e.create(document.getElementById("canvas"),{resize:!0,useWorker:!0})({particleCount:200,spread:200}),console.log(__SNOWPACK_ENV__.SNOWPACK_PUBLIC_ENABLE_FEATURE);
+import * as __SNOWPACK_ENV__ from '../_snowpack/env.js';
+import.meta.env = __SNOWPACK_ENV__;
+
+undefined /* [snowpack] import.meta.hot */ ;
+/**
+ * This file is just a silly example to show everything working in the browser.
+ * When you're ready to start on your site, clear the file. Happy hacking!
+ **/
+
+import confetti from '../_snowpack/pkg/canvas-confetti.js';
+
+confetti.create(document.getElementById('canvas'), {
+  resize: true,
+  useWorker: true,
+})({ particleCount: 200, spread: 200 });
+
+console.log(__SNOWPACK_ENV__.API_URL);
